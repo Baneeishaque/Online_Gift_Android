45ab7edafec5583349d3323ed84bc4e524066cbd
Banee Ishaque K <Baneeishaque@gmail.com>
2018-04-06 14:10:18 +0530

    in

---
+++ app/src/main/java/com/ssmptc/onlinegift/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ssmptc.onlinegift;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView wv=(WebView)findViewById(R.id.wv);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setFocusable(true);
+        wv.setFocusableInTouchMode(true);
+        wv.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        wv.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        wv.getSettings().setDomStorageEnabled(true);
+        wv.getSettings().setDatabaseEnabled(true);
+        wv.getSettings().setAppCacheEnabled(true);
+        wv.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        wv.loadUrl("http://192.168.43.40/medican/Default.aspx");
+        wv.setWebViewClient(new WebViewClient());
+    }
+}
